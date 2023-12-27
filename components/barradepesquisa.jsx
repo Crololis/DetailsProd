78305b98a35afd6e34373ac7435c44206c89fa59
Crololis <145622960+Crololis@users.noreply.github.com>
2023-12-27 14:03:24 -0300

    Add files via upload

---
+++ components/barradepesquisa.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import "../estilos/BarraDePesquisa.sass";
+import search from "../assets/images/search_circle.svg";
+
+const SearchBar = ({ onSearch }) => {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleInputChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const handleSearch = () => {
+    // Verifica se onSearch é uma função antes de chamar
+    if (typeof onSearch === 'function') {
+      onSearch(searchTerm);
+    } else {
+      console.error('onSearch não é uma função válida');
+    }
+  };
+
+  return (
+    <div className='divDaBarra'>
+      <input
+        className='barra'
+        type="text"
+        placeholder="O que seu pet precisa?"
+        value={searchTerm}
+        onChange={handleInputChange}
+      />
+      <button className='botaoPesquisar' onClick={handleSearch}>
+        <img src={search} alt="procurar" />
+      </button>
+    </div>
+  );
+};
+
+export default SearchBar;
